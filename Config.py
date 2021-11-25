ff83dbfe460e33f122081f35dce92874d8eb2874
abdoalissa <35194512+abdoalissa@users.noreply.github.com>
2021-11-25 08:50:02 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,13 @@ ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
+        API_ID = int(os.environ.get('316144', 0))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', None)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+    API_HASH = os.environ.get('61b01fe1d7686f2c6f64ce61aeefea9f', None)
+    BOT_TOKEN = os.environ.get('2135453200:AAFOuVL8aaEAZJQZ5tmmdGDOJZfrvEhyV-s', None)
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 316144
+    API_HASH = "61b01fe1d7686f2c6f64ce61aeefea9f"
+    BOT_TOKEN = "2135453200:AAFOuVL8aaEAZJQZ5tmmdGDOJZfrvEhyV-s"
